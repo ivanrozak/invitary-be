432ecf03dab30e5cf9337b802eaa5ae44faa822b
ivanrozak <ivan.rozak@yesdok.com>
2022-06-15 15:16:47 +0700

    bismillah

---
+++ constants.js
@@ -0,0 +1,15 @@
+const DATABASE_NAME='invitary_db1';
+const USERNAME='root';
+const PASSWORD='root';
+const HOST='localhost';
+const PORT='8889';
+const DIALECT='mysql';
+
+module.exports = {
+  DATABASE_NAME: DATABASE_NAME,
+  USERNAME:USERNAME,
+  PASSWORD:PASSWORD,
+  HOST:HOST,
+  PORT:PORT,
+  DIALECT:DIALECT
+}
